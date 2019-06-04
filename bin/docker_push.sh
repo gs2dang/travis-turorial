f3278a6d442fd36d341ffe034ae236b408a3c28a
gs2dang <gs2dang07@gmail.com>
2019-06-04 23:14:33 +0900

    Permission denied 발생- chmod로 수정 2차 작업

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,4 +1,3 @@
 - docker build -t "$DOCKER_USERNAME"/community .
 - echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-- sudo chmod +x bin/docker_push.sh
 - docker push "$DOCKER_USERNAME"/community:latest
