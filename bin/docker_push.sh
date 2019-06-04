1ce1ab8cf4cc173dd9732d592ea4c896496df6ec
gs2dang <gs2dang07@gmail.com>
2019-06-04 22:50:57 +0900

    더블쿼트 지워도 잘 된느지 확인

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,3 +1,3 @@
-- docker build -t ""$DOCKER_USERNAME"/community .
-- echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-- docker push ""$DOCKER_USERNAME"/community:latest
+- docker build -t $DOCKER_USERNAME/community .
+- echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+- docker push $DOCKER_USERNAME/community:latest
