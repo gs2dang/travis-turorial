b47bf61efd641e4cafa371c28cefa1e5ad13c3bb
gs2dang <gs2dang07@gmail.com>
2019-06-05 17:43:11 +0900

    Environment Variables 확인

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,3 +1,4 @@
-docker build -t "$DOCKER_USERNAME"/community .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_USERNAME"/community:latest
+docker build -t $DOCKER_USERNAME/$REPO_NAME .
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+chmod +x bin/docker_push.sh
+docker push $DOCKER_USERNAME/$REPO_NAME:latest
