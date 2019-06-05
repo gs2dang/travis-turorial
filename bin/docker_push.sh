e20ccc9c7243d32f302b0b865d94fe398842155d
gs2dang <gs2dang07@gmail.com>
2019-06-05 18:07:08 +0900

    chmod 1차 수정 bin/.. -> /bin/..

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,4 +1,4 @@
 docker build -t $DOCKER_USERNAME/$REPO_NAME .
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-chmod +x bin/docker_push.sh
+chmod +x /bin/docker_push.sh
 docker push $DOCKER_USERNAME/$REPO_NAME:latest
