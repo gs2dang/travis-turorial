071694c2a79ecc5bc696fffa3a342f5d0d126133
gs2dang <gs2dang07@gmail.com>
2019-06-05 18:54:13 +0900

    chmod 5차 수정- chmod 설정을 travis.yml로 이동

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,4 +1,3 @@
 docker build -t $DOCKER_USERNAME/$REPO_NAME .
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-sudo chmod +x /usr/bin/docker_push.sh
 docker push $DOCKER_USERNAME/$REPO_NAME:latest
